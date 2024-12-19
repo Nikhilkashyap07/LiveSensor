17d331656d5abb35e28abea525a8bb3d268455fb
Nikhilkashyap07 <nikhillashyap07@gmail.com>
2024-12-19 22:35:14 +0530

    after add e

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+# from typing import List
+
+
+def get_requirements() -> list[str]:
+
+    requrements_list = list[str] = []
+
+    return requrements_list
+
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="Nikhil",
+    author_email="nikhillashyap07@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
